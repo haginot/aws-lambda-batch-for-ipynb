38f74bf5a40ef686f6fc31566e80b82a7da92ab1
Hagino Takahiro <hagino@apache.org>
2022-02-21 01:13:41 +0900

    Pass the service account information of gcp set in the environment variable to the batch notebook for auth

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import os
+from google.oauth2 import service_account
+from pydata_google_auth.cache import CredentialsCache
 import logging
 import json
 import papermill as pm
@@ -6,11 +8,43 @@ import papermill as pm
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)   
 
+service_account_env = os.environ.get('SERVICE_ACCOUNT')
+
 def handler(event, context):
     logger.info(json.dumps(event))
+    service_account_path = None
+    if service_account_env:
+        if os.path.isfile(service_account_env):
+            service_account_path = os.path.abspath(service_account_env)
+        else:
+            service_account_path = '/tmp/service_account.json'
+            service_account_info = (
+                {
+                    "type": "service_account",
+                    "project_id": os.environ.get('SERVICE_ACCOUNT_PROJECT_ID'),
+                    "private_key_id": os.environ.get('SERVICE_ACCOUNT_PRIVATE_KEY_ID'),
+                    "private_key": os.environ.get('SERVICE_ACCOUNT_PRIVATE_KEY'),
+                    "client_email": os.environ.get('SERVICE_ACCOUNT_CLIENT_EMAIL'),
+                    "client_id": os.environ.get('SERVICE_ACCOUNT_CLIENT_ID'),
+                    "auth_uri": os.environ.get('SERVICE_ACCOUNT_AUTH_URI'),
+                    "token_uri": os.environ.get('SERVICE_ACCOUNT_TOKEN_URI'),
+                    "auth_provider_x509_cert_url": os.environ.get('SERVICE_ACCOUNT_AUTH_PROVIDER_X509_CERT_URI'),
+                    "client_x509_cert_url": os.environ.get('SERVICE_ACCOUNT_CLIENT_X509_CERT_URI')
+                }
+            )
+            with open(service_account_path, 'w') as file:
+                json.dump(service_account_info, file)
+
+    # notebook_params = pm.inspect_notebook('src/app/input.ipynb')
+    # logger.info(notebook_params)
+
     pm.execute_notebook(
         'src/app/input.ipynb',
         '/tmp/output.ipynb',
-        parameters=dict()
+        parameters=dict(service_account_path=service_account_path)
     )
-    return {'statusCode': 200, 'body': "{'handler': 'success'}", 'MY_ENV': os.environ.get('MY_ENV')}
+
+    with open('/tmp/test', 'r') as f:
+        result = f.read()
+
+    return {'statusCode': 200, 'body': "{'handler': 'success'}", 'result': result}
--- test.local.sh
+++ test.local.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-DOCKER_PID=$(docker run --rm  -e MY_ENV=hogehoge -d -p 9001:8080  aws-lambda-batch-for-ipynb:latest)
+docker build -t aws-lambda-batch-for-ipynb . 
+DOCKER_PID=$(docker run --rm -e SERVICE_ACCOUNT= -d -p 9001:8080  aws-lambda-batch-for-ipynb:latest)
 curl -XPOST "http://localhost:9001/2015-03-31/functions/function/invocations" -d '{}'
 echo 'docker logs'
 docker logs $DOCKER_PID
